d5ee68016ad6fe01849efe6778854a7f4372ae27
Hussain Al Zerjawi <55470269+babylonianLion@users.noreply.github.com>
2023-08-05 22:54:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+Sure! Here's a simple README.md file for your login application project:
+
+# Simple Login Application
+
+This is a basic web application built with Python Flask that allows users to register, login, view their dashboard, change their password, and logout. The application uses SQLite as the database to store user information and login sessions.
+
+## Prerequisites
+
+Before running the application, make sure you have the following installed:
+
+- Python 3.x
+- Flask
+
+## Getting Started
+
+1. Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/your-username/Hassan_Project.git
+cd Hassan_Project
+```
+
+2. Install the required dependencies:
+
+```bash
+pip install flask
+```
+
+3. Run the application:
+
+```bash
+python login_backend.py
+```
+
+4. Open your web browser and visit `http://127.0.0.1:5000/` to access the login page.
+
+## Functionality
+
+### Login
+
+- Users can log in with their registered username and password.
+- If the login is successful, the user is redirected to their dashboard.
+- If the login fails due to incorrect credentials, an error message is displayed.
+
+### Registration
+
+- Users can register with a unique username, password, email, and full name.
+
+### Dashboard
+
+- Once logged in, users can view their dashboard, which displays their username, email, and last login time.
+
+### Change Password
+
+- Logged-in users can change their password by providing their current password and a new password.
+
+### Logout
+
+- Users can log out to end their session.
+
+## Database
+
+The application uses SQLite as the database. The `login_database.db` file will be created in the same directory when you run the application.
+
+## Important Note
+
+- In this project, passwords are stored as plain text for simplicity. In a real-world application, it's essential to implement proper password hashing for security.
+- For production use, consider deploying the application on a secure web server and set up SSL certificates to ensure secure communication.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Credits
+
+This project is created by [Your Name](https://github.com/your-username).
+
+---
+
+Feel free to customize the README.md file to include any additional information, such as deployment instructions, project structure, or any other relevant details about your project.
---
+++ login_backend.py
@@ -0,0 +1,169 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Secret key for session management
+app.secret_key = 'your_secret_key'
+
+# SQLite database file name
+DB_NAME = "login_database.db"
+
+# Function to create the users table
+def create_users_table():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                username TEXT NOT NULL UNIQUE,
+                password_hash TEXT NOT NULL,
+                email TEXT NOT NULL,
+                full_name TEXT
+            )
+            """
+        )
+        conn.commit()
+
+# Function to create the login_sessions table
+def create_login_sessions_table():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS login_sessions (
+                session_id INTEGER PRIMARY KEY,
+                user_id INTEGER NOT NULL,
+                login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users (user_id)
+            )
+            """
+        )
+        conn.commit()
+
+# Function to insert a new user into the users table
+def insert_user(username, password_hash, email, full_name=None):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO users (username, password_hash, email, full_name) VALUES (?, ?, ?, ?)",
+            (username, password_hash, email, full_name),
+        )
+        conn.commit()
+
+# Function to check login credentials
+def check_login_credentials(username, password):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT user_id, password_hash FROM users WHERE username = ?", (username,))
+        user = cursor.fetchone()
+
+    if user and user[1] == password:
+        return user[0]  # Return the user_id if the password matches
+    else:
+        return None
+
+# Function to fetch user data for the landing page
+def fetch_user_data(user_id):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT username, email, login_time FROM login_sessions JOIN users USING(user_id) WHERE user_id = ? ORDER BY login_time DESC LIMIT 1",
+            (user_id,),
+        )
+        user_data = cursor.fetchone()
+    return user_data
+
+# Function to update the password for a user
+def update_password(user_id, new_password_hash):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE users SET password_hash = ? WHERE user_id = ?", (new_password_hash, user_id))
+        conn.commit()
+
+# Function to create the necessary tables
+def create_database():
+    create_users_table()
+    create_login_sessions_table()
+
+# Routes
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]  # In a real application, you should hash the password securely
+
+        # Check login credentials
+        user_id = check_login_credentials(username, password)
+        if user_id:
+            session["user_id"] = user_id
+
+            # Log the login session
+            with sqlite3.connect(DB_NAME) as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO login_sessions (user_id) VALUES (?)", (user_id,))
+                conn.commit()
+
+            return redirect(url_for("dashboard"))
+        else:
+            return redirect(url_for("login_failed"))
+
+    return render_template("login.html")
+
+@app.route("/login_failed")
+def login_failed():
+    return render_template("login_failed.html")
+
+@app.route("/create_user", methods=["GET", "POST"])
+def create_user():
+    if request.method == "POST":
+        username = request.form["username"]
+        password_hash = request.form["password"]  # In a real application, you should hash the password securely
+        email = request.form["email"]
+        full_name = request.form["full_name"]
+
+        # Insert the new user into the database
+        insert_user(username, password_hash, email, full_name)
+
+        # Redirect to the login page after creating the user
+        return redirect(url_for("login"))
+
+    return render_template("create_user.html")
+
+@app.route("/dashboard")
+def dashboard():
+    user_id = session.get("user_id")
+    if user_id:
+        # Fetch user data for the landing page
+        user_data = fetch_user_data(user_id)
+
+        return render_template("dashboard.html", user_data=user_data)
+
+    # Redirect to login page if the user is not logged in
+    return redirect(url_for("login"))
+
+@app.route("/change_password", methods=["GET", "POST"])
+def change_password():
+    user_id = session.get("user_id")
+    if user_id:
+        if request.method == "POST":
+            new_password_hash = request.form["new_password"]
+            update_password(user_id, new_password_hash)
+            return redirect(url_for("dashboard"))
+
+        return render_template("change_password.html")
+
+    # Redirect to login page if the user is not logged in
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect(url_for("login"))
+
+# Start the Flask application
+if __name__ == "__main__":
+    create_database()  # Create the necessary tables before starting the application
+    app.run(debug=True)
